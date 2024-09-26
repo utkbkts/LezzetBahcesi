b7cc92d9e06b257c010b9ead10fde600c7733391
utku <utkbktss5@gmail.com>
2024-09-26 22:42:23 +0300

    update helmet

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,14 @@ app.use(
   })
 );
 const __dirname = path.resolve();
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"],
+    },
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api", AuthRouters);
