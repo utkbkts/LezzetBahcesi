edf62b281edb9b0c9074acde9d97fc3af4776c5c
utku <utkbktss5@gmail.com>
2024-09-30 13:34:31 +0300

    completed contact

--- frontend/src/pages/contact/ContactPage.jsx
+++ frontend/src/pages/contact/ContactPage.jsx
@@ -6,6 +6,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { contactPage } from "../../utils/validation";
 import { useSendMessageMutation } from "../../redux/api/ContactApi";
 import toast from "react-hot-toast";
+import menu7 from "/menuPage/img-def.png";
+
 import { useEffect } from "react";
 const ContactPage = () => {
   const [sendMessages, { isSuccess, isLoading, error }] =
@@ -35,7 +37,19 @@ const ContactPage = () => {
   };
   return (
     <div className="h-full">
-      <Hero title={"İletişim"} />
+      {/* Hero Section */}
+      <div className="relative">
+        <Hero
+          classNameHero={"bg-contact"}
+          title={"İletişim."}
+          titleClass={"berkshire-swash-regular"}
+          subTitle={"Aklınıza takılan her soruyu sorabilirsiniz."}
+          titleMainClass={"items-center"}
+        />
+        <div className="absolute top-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <img src={menu7} alt="image" />
+        </div>
+      </div>
       <div className="container mx-auto mt-12 mb-12">
         <div className="flex items-center justify-center w-full">
           <img src={contact} alt="" />
